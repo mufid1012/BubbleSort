8704a0c2ae54bbe5ab03696cce98f2e21ee51143
mufid1012 <mufidghibran1012@gmail.com>
2024-03-14 16:59:32 +0700

    Memperbaiki variabel yang salah

--- Insertion.cpp
+++ Insertion.cpp
@@ -18,7 +18,7 @@ void input(){
     cout << endl;
     cout << "====================" << endl;
     cout << "masukan elemen array" << endl;
-    cout << "====================" << endl;
+    cout << "====================" << endl; //
     for (int i = 0; i < n; i++){
         cout << "data ke-" << (i + 1 ) << ": ";
         cin >> arr[i];
